bbc3fed74b9a25146a32e6b398c92868356cbefa
Luan Simões <luan.simoes1@gmail.com>
2018-12-07 11:47:50 -0200

    Estudo sobre filtragem de dados na tabela.

---
+++ js/filtra.js
@@ -0,0 +1,36 @@
+var campofiltro = document.querySelector("#filtrartabela");
+
+campofiltro.addEventListener("input", function(){
+
+    var pacientes = document.querySelectorAll(".paciente");
+
+
+    if(this.value.length > 0)
+    {
+        for (i = 0 ; i < pacientes.length; i++)
+        {
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value, "i" );
+
+            if(!expressao.test(nome))
+            {
+                paciente.classList.add("invisivel");
+            }
+            else
+            {
+                paciente.classList.remove("invisivel");
+            }
+        }
+    }
+    else
+    {
+        for(i = 0; i < pacientes.length; i++)
+        {
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }
+
+});
